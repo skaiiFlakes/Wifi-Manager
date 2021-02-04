b1ab8b3b351fbd1221c956f4c5587ff28061d4c9
Sky Angeles <spangeles24@xs.edu.ph>
2021-02-05 07:22:57 +0800

    Add files via upload

---
+++ show_surrounding_wifi.py
@@ -0,0 +1,19 @@
+import subprocess
+
+content = ""
+results = subprocess.check_output(["netsh", "wlan", "show", "network"])
+results = results.decode("ascii").replace("\r","")
+
+ls = results.split("\n")
+ls = ls[4:]
+ssids = []
+x = 0
+
+while x < len(ls):
+    if x % 5 == 0:
+        ssids.append(ls[x])
+    x += 1
+for item in ssids:
+    content += f"{item}\n"
+
+print(content)
